63b55a187cb494ca344b21fd041d1610853349d2
Arthur Pereira da Silva <72280602+ArthurPdaSilva@users.noreply.github.com>
2023-12-03 20:57:53 -0300

    refactor: refatorando código de tabela e login

--- src/shemas/SearchingInTable/index.ts
+++ src/shemas/SearchingInTable/index.ts
@@ -1,8 +1,5 @@
 import * as Yup from 'yup';
-
-export interface SearchValidation {
-  value: string;
-}
+import { SearchValidation } from '../../types';
 
 export const SearchSchema = Yup.object<SearchValidation>().shape({
   value: Yup.string().required('Digite um valor para pesquisar'),
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export interface UserValidation {
+    email: string;
+    password: string;
+}
+
+export interface SearchValidation {
+    value: string;
+}
