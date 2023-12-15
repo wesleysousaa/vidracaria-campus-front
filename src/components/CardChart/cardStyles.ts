aa8e5d32780e4d209b7db1106c703ab28b19f45f
wesleysousaa <siginimitsig@gmail.com>
2023-12-14 23:18:19 -0300

    feat: dashboard page

---
+++ src/components/CardChart/cardStyles.ts
@@ -0,0 +1,16 @@
+import { CSSProperties } from '@mui/material/styles/createMixins';
+
+export const cardStyles: CSSProperties = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  flexDirection: 'row',
+  padding: '1em 0.5em 1em 0.5em',
+  width: '15em',
+  textAlign: 'end',
+  borderRadius: '0.3em',
+  boxShadow: '0px 10px 15px -3px rgba(0,0,0,0.1)',
+  flex: '1',
+  flexShrink: 0,
+  margin: '0.5em',
+};
