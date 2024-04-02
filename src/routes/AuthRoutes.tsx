46d59f29cbc7042c3cf7cd9e6a73d7eebaa73c2a
Wesley_ <47366440+wesleysousaa@users.noreply.github.com>
2024-04-02 13:02:53 -0300

    fix: refresh token dont work

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material';
 import { Navigate, Outlet } from 'react-router-dom';
 import Menu from '../features/Menu';
-import isAuthenticated from './utils/isAuthenticated';
+import { isAuthenticated } from './utils/isAuthenticated';
 
 export function PrivateRoute() {
   return isAuthenticated() ? (
