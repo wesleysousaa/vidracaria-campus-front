d7f4f8a58ea51a575a3d64b509035db08ac90c6e
Wesley_ <47366440+wesleysousaa@users.noreply.github.com>
2024-05-05 15:28:51 -0300

    refactor: responsivity in line chart

--- src/features/Dashboard/Charts/LineChart/index.tsx
+++ src/features/Dashboard/Charts/LineChart/index.tsx
@@ -62,6 +62,7 @@ export default function LineChart(props: propsLineChart) {
       padding={'1em'}
       boxShadow={'0px 4px 4px rgba(0, 0, 0, 0.25)'}
       flex={1}
+      overflow={'hidden'}
     >
       <Typography variant="h6" textAlign={'center'}>
         {props.title}
