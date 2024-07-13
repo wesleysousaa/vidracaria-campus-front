38370f89dbb9641306fe33d7a2e1dd5cea58401f
Wesley_ <47366440+wesleysousaa@users.noreply.github.com>
2024-07-12 23:33:50 -0300

    feat(return back message): add interceptor to listen errors

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { enqueueSnackbar } from 'notistack';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_URL,
@@ -9,5 +10,20 @@ export const config = {
     Authorization: `Bearer ${localStorage.getItem('token')}`,
   },
 };
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const errorMessage =
+      (error.response && error.response.data) ||
+      'Erro desconhecido ao realizar a operação.';
+    enqueueSnackbar(errorMessage, {
+      variant: 'error',
+    });
+    return Promise.reject(error);
+  },
+);
 
 export default api;
