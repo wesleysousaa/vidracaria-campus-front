e6873a665d6d9bb935f90d4f82d1e95f624b4785
Arthur Pereira da Silva <arthurnpdasilva@gmail.com>
2024-07-27 20:14:42 -0300

    fix

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, redirect } from '@tanstack/react-router';
 export const Route = createFileRoute('/_authenticated')({
   beforeLoad: async ({ context }) => {
     const { isAuthenticated } = context.authentication;
-    if (isAuthenticated()) {
+    if (!isAuthenticated()) {
       throw redirect({ to: '/' });
     }
   },
